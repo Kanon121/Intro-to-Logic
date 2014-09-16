238ba257021e160b1f9d786f916c5139af460a03
Kanon121 <Kanon121@users.noreply.github.com>
2014-09-16 08:58:58 -0400

    Update Logic 3.py

--- Logic 3.py
+++ Logic 3.py
@@ -3,4 +3,56 @@ a = [1, 2, 3, 4]
 b = [1, 2, 5, 6]
 c = [1, 3, 5, 7]
 
+#####
+c^ = []
+a_c = []
+for number in univ:
+  if number not in c:
+    c^.append(number)
+for number in a:
+  if number in c^:
+    a_c.appened(number)
+
+#####
+b_c = []
+for number in b:
+  b_c.append(number)
+for number in c^:
+  if number not in b_c:
+    b_c.append(number)
+
+#####
+b_c = []
+a_c = []
+
+a_c_b_c = []
+for number in b:
+  if number in c:
+    b_c.append(number)
+for number in a:
+  if number in c:
+    a_c.append(number)
+for number in b_c
+  a_c_b_c.append(number)
+for number in a_c:
+  if number not in a_c_b_c:
+    a_c_b_c.append(number)
+#####
+a_b = []
+a_b^ = []
+a_b_c^ = []
+for number in a:
+  a_b.append(number)
+for number in b:
+  if number not in a_b:
+    a_b.append(number)
+for number in univ:
+  if number not in a_b:
+    a_b^.append(number)
+for number in c^:
+  a_b_c^.append(number)
+for number in a_b^:
+  if number not in a_b_c^:
+    a_b^.append(number)
+##### Question 5: 
 
